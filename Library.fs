9c280ed1e583aba3073f080ec46804478614a2a6
Girish Karnik <karnik.girish@gmail.com>
2021-06-29 13:12:03 +0530

    Add files via upload

---
+++ Library.fs
@@ -0,0 +1,22 @@
+﻿namespace FsharpSampleProj1
+
+module Say =
+
+    open FSharp.Data;
+    type FinanceData = HtmlProvider<"https://finance.yahoo.com/quote/^GSPC/history?p=^GSPC">
+    type dateWiseHiLow = {date: string; openingValue: string;High: string;Low:string; close: string;volume: string }
+    let table = FinanceData.Load("https://finance.yahoo.com/quote/^GSPC/history?p=^GSPC")
+                        .Tables
+                        .Table1
+                      
+    let  rows = table.Rows
+                |> Seq.map(fun r->{ date = r.``Date``;
+                                    openingValue = r.Open;
+                                    High= r.``High``;
+                                    Low=r.Low;
+                                    close=r.``Close*``;
+                                    volume=r.Volume
+                                    })
+                |> Seq.toList
+    
+
